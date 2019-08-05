e725c62d87d0e49fc8c3bb5372f5536fbea33b9f
Tkachenko Vladislav <webkadiz@gmail.com>
2019-08-05 19:03:00 +0300

    add strtolower and strtounder in Util class

--- framework/util/Util.php
+++ framework/util/Util.php
@@ -40,4 +40,30 @@ class Util {
         return pathinfo($path, PATHINFO_EXTENSION);
     }
 
+    function strtocamel($str) {
+	    $strParts = explode('_', $str);
+	    $resStr = array_shift($strParts);
+
+	    foreach ($strParts as $strPart) {
+		    $resStr .= ucfirst($strPart);
+	    }
+
+	    return $resStr;
+    }
+
+    function strtounder($str) {
+	    $strChars = str_split($str);
+
+	    $index = 0;
+	    foreach ($strChars as $char) {
+		    if (ctype_upper($char)) {
+			    $str = substr($str, 0, $index) . '_' . strtolower($char) . substr($str, $index + 1);
+			    $index++;
+		    }
+		    $index++;
+	    }
+
+	    return $str;
+    }
+
 }
